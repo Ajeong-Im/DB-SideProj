aff8845763122d88c5e1ded84e6c90d86b24ff14
Ajeong-Im <jenny2360@naver.com>
2023-11-06 11:58:18 +0900

    feat : 페이지 상세 내용 변경

--- src/pages/Guest/GuestPage.tsx
+++ src/pages/Guest/GuestPage.tsx
@@ -8,7 +8,9 @@ const GuestPage = () => {
       </div>
       <div>
         <Link to="/addguest">
-          <button className="bg-slate-300">고객 등록</button>
+          <button className="t-full bg-slate-300 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+            고객 등록
+          </button>
         </Link>
       </div>
     </div>
--- src/pages/Vehicle/CarList.tsx
+++ src/pages/Vehicle/CarList.tsx
@@ -4,6 +4,11 @@ const CarList = () => {
   return (
     <>
       <h3>여긴 Vehicle Page</h3>
+      <Link to={"/addcar"}>
+        <button className="t-full bg-slate-300 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+          차량 추가
+        </button>
+      </Link>
       <ul>
         <Link to={"/product/1"}>
           <li>1번 차</li>
