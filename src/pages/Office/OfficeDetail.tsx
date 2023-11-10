c8790ce8b33aaad17e54c2dda945ab82e7a8583e
Ajeong-Im <jenny2360@naver.com>
2023-11-11 03:42:54 +0900

    feat : 지점 리스트 생성

---
+++ src/pages/Office/OfficeDetail.tsx
@@ -0,0 +1,10 @@
+// OfficeDetail.tsx
+
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const OfficeDetail = () => {
+  return <div></div>;
+};
+
+export default OfficeDetail;
--- src/pages/Office/OfficePage.tsx
+++ src/pages/Office/OfficePage.tsx
@@ -1,9 +1,38 @@
 import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const officeListData = [
+  {
+    id: 1,
+    office_name: "강남점",
+    office_tel: "010-1234-5678",
+    office_addr: "서울시 강남구",
+  },
+  {
+    id: 2,
+    office_name: "역삼점",
+    office_tel: "010-8765-4321",
+    office_addr: "서울시 역삼동",
+  },
+];
 
 const OfficePage = () => {
+  const [officeList, setOfficeList] = useState([]);
+
   return (
     <>
-      <h3>여긴 Office Page</h3>
+      <Link to={"/addoffice"}>
+        <button className="bg-orange-200 mb-3 mt-3">지점 추가</button>
+      </Link>
+      <h3 className="text-lg w-full bg-yellow-200">지점 목록</h3>
+      <ul>
+        {officeListData.map((office) => (
+          <Link key={office.id} to={`/office/${office.id}`}>
+            <li>{`${office.office_name} : ${office.office_tel} 주소: ${office.office_addr}`}</li>
+          </Link>
+        ))}
+      </ul>
     </>
   );
 };
